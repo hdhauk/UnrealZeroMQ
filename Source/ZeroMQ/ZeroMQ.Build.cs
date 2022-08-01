d9cdbd4a08e9987bbbf325ea951f8bdca43cb777
Brantley Harris <deadwisdom@gmail.com>
2022-07-31 22:47:56 -0500

    Remove Win32 / Fix switch issue
    Unreal no longer supports Win32 also can't switch on the platform.

--- Source/ZeroMQ/ZeroMQ.Build.cs
+++ Source/ZeroMQ/ZeroMQ.Build.cs
@@ -26,25 +26,16 @@ public class ZeroMQ : ModuleRules
         PublicDefinitions.Add("ZMQ_STATIC");
 
         string staticLibrary = "";
-        switch (Target.Platform)
-        {
-            case UnrealTargetPlatform.Win64:
-                staticLibrary = Path.Combine(ZeroMQRootPath, "Windows", "x64", "libzmq-v141-mt-s-4_3_2.lib");
-                break;
-            case UnrealTargetPlatform.Win32:
-                staticLibrary = Path.Combine(ZeroMQRootPath, "Windows", "x86", "libzmq-v141-mt-s-4_3_2.lib");
-                break;
-            case UnrealTargetPlatform.Linux:
-                staticLibrary = Path.Combine(ZeroMQRootPath, "Linux", "libzmq.so");
-                PublicAdditionalLibraries.Add("stdc++");
-                break;
-            case UnrealTargetPlatform.Mac:
-                staticLibrary = Path.Combine(ZeroMQRootPath, "MacOS", "libzmq.a");
-                break;
-            default:
-                Console.WriteLine("unsupported target platform: %s", Target.Platform);
-                Debug.Assert(false);
-                break;
+        if (Target.Platform == UnrealTargetPlatform.Win64) {
+            staticLibrary = Path.Combine(ZeroMQRootPath, "Windows", "x64", "libzmq-v141-mt-s-4_3_2.lib");
+        } else if (Target.Platform == UnrealTargetPlatform.Linux) {
+            staticLibrary = Path.Combine(ZeroMQRootPath, "Linux", "libzmq.so");
+            PublicAdditionalLibraries.Add("stdc++");
+        } else if (Target.Platform == UnrealTargetPlatform.Mac) {
+            staticLibrary = Path.Combine(ZeroMQRootPath, "MacOS", "libzmq.a");
+        } else {
+            Console.WriteLine("unsupported target platform: %s", Target.Platform);
+            Debug.Assert(false);
         }
 
         bEnableExceptions = true;
